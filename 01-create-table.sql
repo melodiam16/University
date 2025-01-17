d4346ec8a2cb18ead26688c257f4fa2b81a50a1a
melodiam16 <121240405+melodiam16@users.noreply.github.com>
2025-01-17 19:22:28 +0100

    new tables

--- 01-create-table.sql
+++ 01-create-table.sql
@@ -49,3 +49,41 @@
 	-- 2 спосіб для генерування id 
 	-- ALTER TABLE employee
     -- ALTER COLUMN id SET DEFAULT gen_random_uuid();
+
+
+
+-- Create Table dict_liability
+
+CREATE TABLE dict_liability (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(225) NOT NULL,
+    duties VARCHAR(255) NOT NULL
+);
+
+
+
+CREATE TABLE employee_liability (
+    employee_id UUID NOT NULL,
+    liability_id INTEGER NOT NULL,
+    PRIMARY KEY (employee_id, liability_id),
+    CONSTRAINT fk_employee FOREIGN KEY (employee_id) REFERENCES employee (id) ON DELETE CASCADE,
+    CONSTRAINT fk_liability FOREIGN KEY (liability_id) REFERENCES dict_liability (id) ON DELETE CASCADE
+);
+
+
+--Check employee_liability
+-- el - Це псевдонім для таблиці employee_liability. 
+-- dl- Це псевдонім для таблиці dict_liability.
+-- Використовується, щоб скоротити запис і зробити запит компактнішим.
+
+SELECT 
+    e.name AS employee_name, 
+    e.last_name AS employee_last_name,
+    dl.name AS liability_name,
+    dl.duties AS liability_duties
+FROM 
+    employee_liability el
+JOIN 
+    employee e ON el.employee_id = e.id
+JOIN 
+    dict_liability dl ON el.liability_id = dl.id;
